3c4d651da79997778be18b6fbc5d8ac325e5c602
OmarMallouk <mr.mallouk.om@gmail.com>
2025-03-09 06:33:53 +0200

    added sanity to manage my data

--- app/components/StartupCard.tsx
+++ app/components/StartupCard.tsx
@@ -4,33 +4,53 @@ import { Eye, EyeIcon } from 'lucide-react'
 import Link from 'next/link'
 
 const StartupCard = ({post}:{post: StartupTypeCard}) => {
-    const {_createdAt, views, author:{_id: authorId, name}, title, _id, category} = post;
+    const {_createdAt, views, author:{_id: authorId, image, name}, title, description, _id, category} = post;
   return (
-    <li className='startup-card group'>
-        <div className='flex-between'>
-            <p className='startup_card_date'>
-                {formatDate(_createdAt)}
-            </p>
-            <div className='flex gap-1.5'>
-                <EyeIcon className='size-6 text-primary'/>
-                <span className='text-16-medium'>{views}</span>
-            </div>
+    <li className="startup-card group">
+    <div className="flex-between">
+      <p className="startup_card_date">{formatDate(_createdAt)}</p>
+      <div className="flex gap-1.5">
+        <EyeIcon className="size-6 text-primary" />
+        <span className="text-16-medium">{views}</span>
+      </div>
+    </div>
 
-            <div className='flex-between mt-5 gap-5'>
-                <div className='flex-1'>
-                    <Link href={`/user/${authorId}`}>
-                    <p className='text-16-medium line-clamp-1'>
-                        {name}</p></Link>
+    <div className="flex-between mt-5 gap-5">
+      <div className="flex-1">
+        <Link href={`/user/${authorId}`}>
+          <p className="text-16-medium line-clamp-1">{image}</p>
+        </Link>
+        <Link href={`/startup/${_id}`}>
+          <h3 className="text-26-semibold line-clamp-1">{title}</h3>
+        </Link>
+      </div>
+      <Link href={`/user/${authorId}`}>
+        <img
+          src={image}
+          alt={name}
+          width={48}
+          height={48}
+          className="rounded-full"
+        />
+      </Link>
+    </div>
 
-                    <Link href={`/startup/${_id}`}>
-                    <h3 className='text-26-semibold line-clamp-1'>
-                        {title}
-                        </h3></Link>
-                </div>
-            </div>
-        </div>
-    </li>
-  )
-}
+    <Link href={`/startup/${_id}`}>
+      <p className="startup-card_desc">{description}</p>
+
+      <img src={image} alt="placeholder" className="startup-card_img" />
+    </Link>
+
+    <div className="flex-between gap-3 mt-5">
+      <Link href={`/?query=${category?.toLowerCase()}`}>
+        <p className="text-16-medium">{category}</p>
+      </Link>
+      <button className="startup-card_btn">
+        <Link href={`/startup/${_id}`}>Details</Link>
+      </button>
+    </div>
+  </li>
+);
+};
 
 export default StartupCard
